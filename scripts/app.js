01b4ffd3359c54c9e533b4be8bafeb3557c97a0b
Alan Tse <starttodevelop@gmail.com>
2017-06-09 14:26:10 +1000

    Initial Commit

---
+++ scripts/app.js
@@ -0,0 +1,33 @@
+console.log("App started!");
+
+/*
+var toggleButton = document.getElementById("menu-button");
+var eventType = "click";
+var mainHeading = document.getElementById("main-heading");
+var navBar = document.getElementById("nav-bar");
+var toggleButtonIsClicked = false;
+
+toggleButton.addEventListener(eventType, toggleButtonClicked);
+window.addEventListener("resize", resize);
+
+function toggleButtonClicked() {
+    if (toggleButtonIsClicked) {
+        resize();
+        
+
+    } else {
+
+        mainHeading.innerHTML = "Toggle Button was clicked";
+        mainHeading.style.color = "red";
+        navBar.className = navBar.className + " responsive-nav";
+        toggleButtonIsClicked = true;
+    }
+}
+
+function resize() {
+    toggleButtonIsClicked = false;
+    mainHeading.innerHTML = "Main Heading";
+    mainHeading.style.color = "black";
+    navBar.className = "nav-bar";
+}
+*/
